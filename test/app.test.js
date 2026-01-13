6f4a54a80b2502f07213e58a09b35f74e1eddc13
Uditjain193 <uditjain198@gmail.com>
2026-01-14 04:32:58 +0530

    updates

--- test/app.test.js
+++ test/app.test.js
@@ -2,9 +2,9 @@ const request = require("supertest");
 const express = require("express");
 
 const app = express();
-app.get("/", (req, res) => res.send("CI/CD Pipeline Working 🚀"));
+app.get("/", (req, res) => res.send("CI/CD Pipeline Working "));
 
 test("GET / should return success", async () => {
   const res = await request(app).get("/");
-  expect(res.text).toBe("CI/CD Pipeline Working 🚀");
+  expect(res.text).toBe("CI/CD Pipeline Working ");
 });
